d262750e4ec8cfb1754eb0d0989f61a497dc43e4
Roy <rorocoffeee@gmail.com>
2020-12-07 00:02:44 +0100

    first push

---
+++ Source/FGNet/FGNetGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FGNetGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FGNET_API AFGNetGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
